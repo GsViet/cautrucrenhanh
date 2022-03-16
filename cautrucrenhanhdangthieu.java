37f594f1ebb16f630cc51d39041417887e65b99a
vietGS <giuseviet1411@gmail.com>
2022-03-16 11:21:43 +0700

    first commit

---
+++ cautrucrenhanhdangthieu.java
@@ -0,0 +1,8 @@
+public class cautrucrenhanhdangthieu{
+
+     public static void main(String []args){
+        String s = "Kteam";
+        if (s == "Kteam") 
+        System.out.print("How Kteam");
+     }
+}
